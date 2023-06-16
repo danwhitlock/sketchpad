b0bc085a35b4dad5180d62b466ad944179ece206
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-16 14:43:34 +0100

    create 16x16 grid with hover effect and reset button

--- script.js
+++ script.js
@@ -2,14 +2,28 @@
 
 const container = document.getElementById('grid-container');
 container.style.gridTemplate = 'repeat(16, 1fr) / repeat(16, 1fr)'
-container.style.maxWidth = 'calc(50px * 16)';
+container.style.maxWidth = 'calc(25px * 16)';
 
 for (let i = 0; i <16; i++) {
     for (let j = 0; j <16; j++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
-        div.style.width = '50px';
-        div.style.height = '50px';
+        div.style.width = '25px';
+        div.style.height = '25px';
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = 'black';
+        });
         container.appendChild(div);
     }
 }
+
+const gridItems = document.querySelectorAll('.grid-item');
+const clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', function() {
+  gridItems.forEach(function(item) {
+    item.style.backgroundColor = '';
+  });
+});
+
+
